77af82ab1d513170a46382032775fa570d05ad02
Eman Kashif <emankashif01@gmail.com>
2023-04-05 21:25:31 +0500

    Initial commit

---
+++ lib/onboarding_contents.dart
@@ -0,0 +1,31 @@
+class OnboardingContents {
+  final String title;
+  final String image;
+  final String desc;
+
+  OnboardingContents({
+    required this.title,
+    required this.image,
+    required this.desc,
+  });
+}
+
+List<OnboardingContents> contents = [
+  OnboardingContents(
+    title: "Your friendly messaging buddy!",
+    image: "assets/plutobot.png",
+    desc: "Practice your math skills with pluto",
+  ),
+  OnboardingContents(
+    title: "Chat and learn math at the same time!",
+    image: "assets/calculating.png",
+    desc:
+    "Chat with pluto as you wish. When you want to start practicing, tell pluto. You can ask for your scores too!"
+  ),
+  OnboardingContents(
+    title: "Don't forget to have fun!",
+    image: "assets/think.png",
+    desc:
+    "Math doesn't have to be boring! Ask pluto about it's life!",
+  ),
+];
